af0890515e511a72e0d39d706ac9acadc8a59685
Frederic Petrot <frederic.petrot@grenoble-inp.fr>
2020-11-02 14:59:33 +0100

    Update setup-ensimag.sh so that it works at Ensimag

--- TPs/setup-ensimag.sh
+++ TPs/setup-ensimag.sh
@@ -1,8 +1,4 @@
 #! /bin/echo Non, il ne faut pas executer ce fichier, mais faire: source $0
-
-# Chemin d'installation de SystemC, tjs 2.3.2 en attendant les droits 
-# de mise à jour de /matieres/5MMMTSP
-# export SYSTEMCROOT=/matieres/5MMMTSP/tlm/systemc-2.3.2/
-export SYSTEMCROOT=/tmp/systemc-2.3.3
+export SYSTEMCROOT=/matieres/5MMMTSP/cours-tlm/systemc-2.3.3
 # Réutilisation du cross-compilateur riscv de 4MMPCSEA
 export PATH=/matieres/4MMPCSEA/riscv/bin:$PATH
