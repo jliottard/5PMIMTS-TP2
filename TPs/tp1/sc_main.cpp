99b7d1b2a0e88aff3eb2b0e8d533280a1bcefe13
Julien Liottard <julien.liottard@grenoble-inp.org>
2023-01-07 22:11:25 +0100

    q7-done

--- TPs/tp1/sc_main.cpp
+++ TPs/tp1/sc_main.cpp
@@ -13,15 +13,18 @@ int sc_main(int argc, char **argv)
 
 	Generator generator("Generator1");
 	Bus bus("Bus");
-	uint32_t memory_size_in_byte = 0x100;
+	uint32_t embedded_code_memory_size_in_byte = 10240;
+	uint32_t video_memory_size_in_byte = 340 * 240;
+	uint32_t memory_size_in_byte = video_memory_size_in_byte + video_memory_size_in_byte;
 	Memory memory("Memory", memory_size_in_byte);
 
-	bus.map(memory.target, 0x10000000, memory_size_in_byte + 1);
+	bus.map(memory.target, 0x0, memory_size_in_byte);
 	generator.initiator.bind(bus.target);
 	bus.initiator.bind(memory.target);
 
 	LCDC lcdc("LCDC", sc_time(1.0 / 25, SC_SEC));
-	bus.map(lcdc.target_socket, 0x20000000, 0x100 + 1);
+	uint32_t lcdc_address_range_in_byte = 32 * 3;
+	bus.map(lcdc.target_socket, memory_size_in_byte + 1, lcdc_address_range_in_byte);
 	lcdc.initiator_socket.bind(bus.target);
 	lcdc.target_socket.bind(bus.initiator);
 	
